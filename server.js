d37e88ce1979abd0db236bfac0fe72a0996962b1
prawin.pundrothu@gmail.com <prawin.pundrothu@gmail.com>
2021-11-23 07:02:30 +0000

    Port change to 8083

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: err.message });
 });
 
-const PORT = process.env.PORT ? process.env.PORT : 8082;
+const PORT = process.env.PORT ? process.env.PORT : 8083;
 const server = app.listen(PORT, () => {
     const host = server.address().address;
     const port = server.address().port;
